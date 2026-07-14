df2f3d1523769d93b8b8c391b614225f834c5ee0
Varun Malewar <114666832+Varunmalewar@users.noreply.github.com>
2026-07-14 16:00:37 +0530

    Time: 23 ms (54.54%), Space: 17.4 MB (84.51%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,35 +1,94 @@
+// class Solution {
+// public:
+//     vector<vector<int>> fourSum(vector<int>& nums, int target) {
+//         vector<vector<int>>ans;
+//         int n = nums.size();
+//         sort(nums.begin(),nums.end());
+//         for(int i = 0 ; i < n; i++ ){
+//             if(i > 0 && nums[i] == nums[i-1]) continue;
+                
+            
+//             for( int j= i+1; j < n;){
+//                 int p = j+1, q = n-1;
+//                 while(p<q){
+//                     long long sum = (long long)nums[i]+(long long)nums[j]+(long long)nums[p]+(long long)nums[q];
+
+//                     if( sum > target){
+//                         q--;
+//                     }else if(sum < target){
+//                         p++;
+//                     }else{
+//                         ans.push_back({nums[i],nums[j],nums[p],nums[q]});
+//                         p++;q--;
+//                     while( p < q && nums[p] == nums[p-1]){
+//                         p++;
+//                     }
+//                     }
+
+//                 }
+//                 j++;
+//                 while(j < n && nums[j] == nums[j-1]){j++;}
+//             }
+//         }
+//         return ans;
+//     }
+// };
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        vector<vector<int>>ans;
+
+        vector<vector<int>> ans;
         int n = nums.size();
-        sort(nums.begin(),nums.end());
-        for(int i = 0 ; i < n; i++ ){
-            if(i > 0 && nums[i] == nums[i-1]) continue;
-                
-            
-            for( int j= i+1; j < n;){
-                int p = j+1, q = n-1;
-                while(p<q){
-                    long long sum = (long long)nums[i]+(long long)nums[j]+(long long)nums[p]+(long long)nums[q];
 
-                    if( sum > target){
+        if (n < 4)
+            return ans;
+
+        sort(nums.begin(), nums.end());
+
+        for (int i = 0; i < n - 3; i++) {
+
+            // Skip duplicate i
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+
+            for (int j = i + 1; j < n - 2; j++) {
+
+                // Skip duplicate j
+                if (j > i + 1 && nums[j] == nums[j - 1])
+                    continue;
+
+                int p = j + 1;
+                int q = n - 1;
+
+                while (p < q) {
+
+                    long long sum = 1LL * nums[i] + nums[j] + nums[p] + nums[q];
+
+                    if (sum < target) {
+                        p++;
+                    }
+                    else if (sum > target) {
                         q--;
-                    }else if(sum < target){
-                        p++;
-                    }else{
-                        ans.push_back({nums[i],nums[j],nums[p],nums[q]});
-                        p++;q--;
-                    while( p < q && nums[p] == nums[p-1]){
-                        p++;
                     }
-                    }
+                    else {
 
+                        ans.push_back({nums[i], nums[j], nums[p], nums[q]});
+
+                        p++;
+                        q--;
+
+                        // Skip duplicate p
+                        while (p < q && nums[p] == nums[p - 1])
+                            p++;
+
+                        // Skip duplicate q
+                        while (p < q && nums[q] == nums[q + 1])
+                            q--;
+                    }
                 }
-                j++;
-                while(j < n && nums[j] == nums[j-1]){j++;}
             }
         }
+
         return ans;
     }
 };
