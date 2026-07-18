ccf6d23e0cb4fb3f6a62f20aa8d5da32d8399998
Varun Malewar <114666832+Varunmalewar@users.noreply.github.com>
2026-07-18 12:21:04 +0530

    Time: 5 ms (29.03%), Space: 14.1 MB (58.68%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,36 @@
+class Solution {
+
+public:
+
+    void getsum(vector<int>&arr, int index ,vector<int>&output, int n , int target, vector<vector<int>>&ans){
+        if(target == 0){
+            ans.push_back({output});
+            return;
+        }
+        if(index == n) return;
+        if(target<0) return;
+
+        output.push_back(arr[index]);
+        //single inclusion 
+        // getsum(arr,index+1,output,n , target-arr[index],ans);
+
+        //multiple inclusion 
+        getsum(arr,index ,output, n ,target - arr[index],ans);
+
+        //excluion 
+        output.pop_back();
+        getsum(arr,index+1,output,n,target,ans);
+    }
+
+
+    vector<vector<int>> combinationSum(vector<int>& arr, int target) {
+       vector<vector<int>>ans;
+       vector<int>output;
+       int n = arr.size();
+       int index = 0;
+       getsum(arr,index,output,n,target,ans);
+       return ans ;
+
+
+    }
+};
