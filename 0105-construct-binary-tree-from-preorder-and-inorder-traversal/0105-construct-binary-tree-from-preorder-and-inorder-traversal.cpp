c5526bc0a545b846c5a701bce86749083c2135cf
Varun Malewar <114666832+Varunmalewar@users.noreply.github.com>
2026-08-03 16:18:36 +0530

    Time: 0 ms (100%), Space: 27.5 MB (33.48%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -11,32 +11,51 @@
  */
 class Solution {
 public:
-    int search(vector<int>&inorder, int left , int right , int target){
-        for(int i = left; i<=right; i++){
-            if(inorder[i] == target) {
-                return i;
-            }
+unordered_map<int,int>m;
+//     int search(vector<int>&inorder, int left , int right , int target){
+//         for(int i = left; i<=right; i++){
+//             if(inorder[i] == target) {
+//                 return i;
+//             }
         
-    }
-            return -1;
- }
-    TreeNode *helper(vector<int>& preorder, vector<int>& inorder, int &preIdx,int left, int right){
+//     }
+//             return -1;
+//  }
+    // TreeNode *helper(vector<int>& preorder, vector<int>& inorder, int &preIdx,int left, int right){
+    //     if(left > right) return NULL;
+
+    //     TreeNode *root = new TreeNode(preorder[preIdx]);
+
+    //     int intidx = search(inorder, left , right, preorder[preIdx]);
+
+    //     preIdx++;
+
+    //     root->left = helper(preorder, inorder, preIdx, left , intidx - 1);
+    //     root->right = helper(preorder, inorder, preIdx , intidx + 1, right);
+
+    //     return root;
+    // }
+    TreeNode* helper(vector<int>&preorder, vector<int>&inorder ,int left, int right, int &preIdx){
         if(left > right) return NULL;
 
-        TreeNode *root = new TreeNode(preorder[preIdx]);
-
-        int intidx = search(inorder, left , right, preorder[preIdx]);
-
+        int rootvalue = preorder[preIdx];
+        TreeNode* root = new TreeNode(rootvalue);
         preIdx++;
 
-        root->left = helper(preorder, inorder, preIdx, left , intidx - 1);
-        root->right = helper(preorder, inorder, preIdx , intidx + 1, right);
+        int mid = m[root->val];
+
+        root->left = helper(preorder, inorder, left , mid-1, preIdx);
+        root->right = helper(preorder, inorder , mid+1, right , preIdx);
 
         return root;
     }
 
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
         int preIdx = 0;
-        return helper(preorder, inorder,preIdx,0, inorder.size()-1 );
+        for(int i = 0; i<=inorder.size() -1 ; i++){
+            m[inorder[i]] = i;
+        }
+        return helper(preorder,inorder, 0 , inorder.size() -1, preIdx);
+        
     }
 };
